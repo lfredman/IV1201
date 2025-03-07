10f63a0b2091a0314210cf61e023d855dfb48f92
lfredman <74681135+lfredman@users.noreply.github.com>
2025-03-07 15:21:44 +0100

    Apply for job fixes

--- frontend/src/pages/ApplyForJob.tsx
+++ frontend/src/pages/ApplyForJob.tsx
@@ -3,7 +3,7 @@ import ApplyForAJob from "../components/ApplyForAJob"; // Import the form
 import { useUser } from "../context/UserContext";
 import UserSchedule from "../components/UserSchedule";
 import DateRangeScheduler from "../components/AvailabilityForm";
-import { Box, Typography, Container } from "@mui/material";
+import { Box, Typography, Container, Grid } from "@mui/material";
 import AddCompetence from "../components/AddCompetence";
 import UserCompetences from "../components/UserCompetences";
 import ApplicationForm from "../components/ApplicationForm";
@@ -17,27 +17,35 @@ const ApplyForJob: React.FC = () => {
   const { user } = useUser();
 
   return (
-    <Container>
+    <Container sx={{ mt: 4 }}>
       {user ? (
         <Box>
-          <Typography variant="h4" align="center" gutterBottom>
-            Welcome to apply for a job {user.name}
+          <Typography variant="h4" align="center" gutterBottom sx={{ fontWeight: 600 }}>
+            Welcome to apply for a job, {user.name}
           </Typography>
-          <Typography variant="h6" align="center" gutterBottom>
-            Start fill your competence & availability
+          <Typography variant="h6" align="center" gutterBottom sx={{ mb: 4 }}>
+            Start filling in your competencies & availability
           </Typography>
-          <ApplicationForm/>
-          <UserCompetences editable={false} />
 
-          
-          <DateRangeScheduler />
+          <Grid container spacing={4} justifyContent="center">
+            <Grid item xs={12} sm={8} md={6}>
+              <ApplicationForm />
+            </Grid>
+
+            <Grid item xs={12} sm={8} md={6}>
+              <UserCompetences editable={false} />
+            </Grid>
+          </Grid>
+
+          <Box sx={{ mt: 4 }}>
+            <DateRangeScheduler />
+          </Box>
         </Box>
       ) : (
-        <Typography variant="h4" align="center" gutterBottom>
-          You must log in in first to apply for a job
+        <Typography variant="h5" align="center" sx={{ fontWeight: 500, color: 'gray' }}>
+          You must log in first to apply for a job
         </Typography>
       )}
-
     </Container>
   );
 };
